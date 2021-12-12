4522d7af62bd2bee5753324d48cc9206a9ca27e2
유민기 <mgyou@nextree.io>
2021-12-12 17:34:08 +0900

    Initial commit

---
+++ src/main/java/com/example/jpa_h2_demo/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.jpa_h2_demo;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(JpaH2DemoApplication.class);
+    }
+
+}
